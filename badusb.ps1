81f9ec087b917b1ffb442a49a0916c7dba712bd4
KKlotzBFX <kkl@belmaflex.com>
2025-11-17 09:30:59 +0100

    Add files via upload

--- badusb.ps1
+++ badusb.ps1
@@ -1,3 +1,3 @@
-iwr http://192.168.180.22/RFID/r.php;
 $wshell = New-Object -ComObject Wscript.Shell;
 $Output = $wshell.Popup("UWAGA! Urządzenie USB należy do Belmaflex Polska Sp. z o.o. Zdarzenie zostało zarejestrowane w systemie bezpieczeństwa IT. Prosimy o niezwłocznie przekazanie urządzenia do działu IT Belmaflex Polska.");
+iwr http://192.168.180.22/RFID/r.php;
