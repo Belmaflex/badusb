1acea5bec003a5d14e07f505d50d1428fd4b95ed
KKlotzBFX <kkl@belmaflex.com>
2026-02-18 14:18:35 +0100

    Update badusb.ps1

--- badusb.ps1
+++ badusb.ps1
@@ -1,4 +1,5 @@
 $wshell = New-Object -ComObject Wscript.Shell;
-$Output = $wshell.Popup("UWAGA! `nUrządzenie USB należy do Belmaflex Polska Sp. z o.o. Zdarzenie zostało zarejestrowane w systemie bezpieczeństwa IT. `nPodłączenie nieautoryzowanego nośnika może stanowić zagrożenie – istnieje ryzyko zainfekowania komputera lub wycieku danych. `nProsimy o natychmiastowe odłączenie urządzenia i przekazanie go do działu IT");
+$Output = $wshell.Popup("UWAGA! `nUrządzenie USB należy do Belmaflex Polska Sp. z o.o. Zdarzenie zostało zarejestrowane w systemie bezpieczeństwa IT. `n `n Podłączenie nieautoryzowanego nośnika może stanowić zagrożenie – istnieje ryzyko zainfekowania komputera lub wycieku danych. `nProsimy o natychmiastowe odłączenie urządzenia i przekazanie go do działu IT");
 
 Start-Job { iwr http://192.168.180.22/RFID/r.php }
+
